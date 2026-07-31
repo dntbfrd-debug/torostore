ca8b9b4ea7fe5d0883e693127d9a06c67827717a
snbld <dntbfrd@gmail.com>
2026-07-31 13:10:41 +0300

    Remove hardcoded Telegram IDs

--- server/torostore.ru/stone/config.py
+++ server/torostore.ru/stone/config.py
@@ -1,15 +1,15 @@
 import os
 
 BOT_TOKEN_STONE = os.environ.get('BOT_TOKEN_STONE', os.environ.get('BOT_TOKEN', ''))
-ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID', '2114966435')
+ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID', os.environ.get('ADMIN_CHAT_ID', ''))
 SELLER_CHAT_ID = os.environ.get('SELLER_CHAT_ID') or ADMIN_CHAT_ID
 ADMIN_PASSWORD_STONE = os.environ.get('ADMIN_PASSWORD', '')
 if not ADMIN_PASSWORD_STONE:
     print('[stone] WARNING: ADMIN_PASSWORD not set, admin panel disabled')
 
 BASE_URL = os.environ.get('BASE_URL', 'https://torostore.ru')
-TG_CHANNEL = os.environ.get('TG_CHANNEL', '@storetoro')
-TG_SELLER = os.environ.get('TG_SELLER', '@denayme')
+TG_CHANNEL = os.environ.get('TG_CHANNEL', os.environ.get('TG_CHANNEL', ''))
+TG_SELLER = os.environ.get('TG_SELLER', os.environ.get('TG_SELLER', ''))
 
 PRODUCTS_PER_PAGE = int(os.environ.get('PRODUCTS_PER_PAGE', '50'))
 ARCHIVE_PER_PAGE = int(os.environ.get('ARCHIVE_PER_PAGE', '200'))
